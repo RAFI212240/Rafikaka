c20d180b481f7960bd932d1a7c594f520d6cc4e2
RAFI212240 <islamrak045@gmail.com>
2025-07-10 16:43:21 +0600

    Update Goat.js

--- Goat.js
+++ Goat.js
@@ -1,26 +1,9 @@
-/**
- * @// Goat.js এর শুরুতে যোগ করুন
+// Goat.js এর শুরুতে যোগ করুন
 if (global.GoatBot && global.GoatBot.fcaApi) {
   console.log("Bot already running with active API connection!");
   process.exit(0);
 }
- NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
- */
+
 
 process.on('unhandledRejection', error => console.log(error));
 process.on('uncaughtException', error => console.log(error));
