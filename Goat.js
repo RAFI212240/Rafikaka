8884f4b1cecd61b2c9d9bc256c57ad239d7b1c7a
RAFI212240 <islamrak045@gmail.com>
2025-07-10 16:42:42 +0600

    Update Goat.js

--- Goat.js
+++ Goat.js
@@ -1,5 +1,10 @@
 /**
- * @author NTKhang
+ * @// Goat.js এর শুরুতে যোগ করুন
+if (global.GoatBot && global.GoatBot.fcaApi) {
+  console.log("Bot already running with active API connection!");
+  process.exit(0);
+}
+ NTKhang
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
