9b2f4e5819c89a40d550e51ca898d0a75e30c690
RAFI212240 <islamrak045@gmail.com>
2025-07-12 13:02:10 +0600

    Update test.js

--- scripts/cmds/test.js
+++ scripts/cmds/test.js
@@ -1,39 +1,52 @@
-const { createCanvas, loadImage } = require("canvas");
-const axios = require("axios");
-const fs = require("fs");
+const fs = require("fs-extra");
+const request = require("request");
 const path = require("path");
-const crypto = require("crypto");
 
-module.exports = {
-  config: {
-    name: "flatpfp",
-    version: "1.0",
-    author: "Perplexity AI",
-    description: "Generate a flat style cover+profile image like the sample!",
-    category: "media",
-    cooldowns: 5,
-    guide: "Use {pn}flatpfp <cover_image_url> <profile_image_url>",
-  },
+module.exports.config = {
+  name: "test",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Abdulla Rahaman",
+  description: "Show admin info",
+  commandCategory: "info",
+  usages: "",
+  cooldowns: 1
+};
 
-  onStart: async function ({ message, args }) {
-    if (args.length < 2) {
-      return message.reply(
-        "❌ Please provide cover and profile image URLs.\nExample:\nflatpfp <cover_url> <profile_url>"
-      );
-    }
+module.exports.run = async function({ api, event }) {
+  const links = [
+    "https://i.imgur.com/0Z6GQvF.jpg",
+    "https://i.imgur.com/3g7nmJC.jpg"
+  ];
+  const imgURL = links[Math.floor(Math.random() * links.length)];
+  const filePath = path.join(__dirname, "admin.jpg");
 
-    const coverUrl = args[0];
-    const profileUrl = args[1];
+  await new Promise((resolve, reject) => {
+    request(imgURL)
+      .pipe(fs.createWriteStream(filePath))
+      .on("finish", resolve)
+      .on("error", reject);
+  });
 
-    // Load images
-    let coverImg, profileImg;
-    try {
-      coverImg = await loadImage(coverUrl);
-      profileImg = await loadImage(profileUrl);
-    } catch (e) {
-      return message.reply("❌ Couldn't load one or both images. Please check the URLs.");
-    }
+  const body = `𝗗𝗢 𝗡𝗢𝗧 𝗧𝗥𝗨𝗦𝗧 𝗧𝗛𝗘 𝗕𝗢𝗧 𝗢𝗣𝗘𝗥𝗔𝗧𝗢𝗥
+------------------------------------------------
+𝗡𝗮𝗺𝗲       : R A F Iメ
+𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :   RAFI 卝 চৌধুরীヅ
+𝗥𝗲𝗹𝗶𝗴𝗶𝗼𝗻    : (𝗜𝘀𝗹𝗮𝗺)
+𝗣𝗲𝗿𝗺𝗮𝗻𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : (Rungpur )
+𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : Dhaka, 𝘽𝙖𝙣𝙜𝙡𝙖𝙙𝙚𝙨𝙝
+𝗚𝗲𝗻𝗱𝗲𝗿     : (𝗠𝗮𝗹𝗲)
+𝗔𝗴𝗲            :  (19)
+𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻𝘀𝗵𝗶𝗽 : (biye korle Invitation dimu ni-)
+𝗪𝗼𝗿𝗸         : 𝙎𝙩𝙪𝙙𝙮
+𝗚𝗺𝗮𝗶𝗹        :  private
+𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :  private 😑
+𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺  : t.me/R_A_F_I_Official
+𝗙𝗯 𝗹𝗶𝗻𝗸   : https://www.facebook.com/share/16BbdkmzJo/`;
 
-    // Canvas settings
-    const width = 700, height
-    
+  api.sendMessage({
+    body,
+    attachment: fs.createReadStream(filePath)
+  }, event.threadID, () => fs.unlinkSync(filePath), event.messageID);
+};
+                    
